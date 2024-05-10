227cd07ecddc290f5cb7eaaafe7a950e0e46582b
l6898 <espinolalourdes83@gmail.com>
2024-05-10 13:52:37 +0200

    task

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from  flask import Flask,render_template
-
+from  flask import Flask,render_template 
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///database/tasks.db'
+db = SQLAlchemy(app)
 
 @app.route("/")
 def principal():
-    return render_template('index.html')
+    return render_template('tasks.html')
 
 @app.route("/miembros")
 def verMimebros():
@@ -19,3 +22,5 @@ def verejEmplo():
 
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
+
+
