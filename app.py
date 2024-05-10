8db810021851c31ad91a6ca3049ea8c47d1c24d2
jhonsdb <davilajhon08@gmail.com>
2024-05-10 02:32:13 +0200

    avances en proyecto

---
+++ app.py
@@ -0,0 +1,21 @@
+from  flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def principal():
+    return render_template('index.html')
+
+@app.route("/miembros")
+def verMimebros():
+    equipo=('Jhon','Lourdes','Jesus')
+    return render_template('miembros.html', personas=equipo)
+
+@app.route("/actividadJS")
+def verejEmplo():
+    return render_template('actividad_js.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5000)
