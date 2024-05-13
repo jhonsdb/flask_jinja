844a1b44475915e9ad0003375c3aaffd3b4acdbc
l6898 <espinolalourdes83@gmail.com>
2024-05-13 11:26:44 +0200

    cambios en task

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from  flask import Flask,render_template 
+from  flask import Flask,render_template, request 
 from flask_sqlalchemy import SQLAlchemy
-import sqlite3 
+
+
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///database/tasks.db'
 db = SQLAlchemy(app)
+
 #modelo para la tarea
-class Task(db.model):
-    id = db.column(db.integer, primary_key=True)
-    content= db.column(db.string(200))
-    done = db.column(db.boolean)
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content= db.Column(db.String(200))
+    done = db.Column(db.Boolean)
 
 
 @app.route("/")
@@ -28,10 +32,16 @@ def verejEmplo():
 def formulario():
     return render_template('form.html')
 
+
+
+
 if __name__ == '__main__':
     app.run(debug=True,port=5000)
 
 #ruta para recibir los datos, se tiene que acoplar a la que esta??
 @app.route('/create-task', methods=['POST'])
 def create():
-    Task()
+    task =Task(content =request.form['content'], done = False)
+    #db.session.add(task)
+    #db.session.commit()
+    #return 'save'
